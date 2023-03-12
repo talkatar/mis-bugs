17a6bfffbd91eaac3f90ab92c034bfc13690be58
Tal Katar <talkatar120@gmail.com>
2023-03-12 14:20:08 +0200

    Dfg

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030
 
 const bugService = require('./services/bug.service')
 const userService = require('./services/user.service')
